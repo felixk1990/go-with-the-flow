eacf348d16c98ef0b2ddd40cfe363b2aec592555
felix <felixuwekramer@protonmail.com>
2022-06-28 13:07:24 +0200

    fix test

--- goflow/test/test_modelMurray.py
+++ goflow/test/test_modelMurray.py
@@ -14,35 +14,64 @@ from aux import *
 def initEval():
 
     #initialize circuit+flow pattern
-    Circuit = kfc.initialize_flow_circuit_from_crystal('hexagonal',3)
-    pars_src = dict(mode='default')
-    pars_plx = dict(mode='default')
-    flowArgs= [Circuit, pars_src, pars_plx]
-    F = Flow(*flowArgs)
+    # Circuit = kfc.initialize_flow_circuit_from_crystal('hexagonal',3)
+    # pars_src = dict(mode='default')
+    # pars_plx = dict(mode='default')
+    # flowArgs= [Circuit, pars_src, pars_plx]
+    # F = Flow(*flowArgs)
+    #
+    # # set model and model parameters
+    # pars_model={
+    #     'alpha_0':1.,
+    #     'alpha_1':1.
+    # }
+    # murrayModel = murray(pars_model)
+    #
+    # # # initialize dynamic system and set integration parameters
+    # morpheus = gi.morph_dynamic(flow=F, model=murrayModel)
+    #
+    # cnd = Circuit.edges['conductivity']
+    # cnd_scale = Circuit.scales['conductance']
+    # sp={
+    #     't0': 0.,
+    #     't1': 25.,
+    #     'x0': np.power(cnd/cnd_scale,0.25),
+    # }
+    #
+    # # numerically evaluate the system
+    # update = murrayModel.calc_update_stimuli
+    # nsol = morpheus.nsolve(update, (sp['t0'],sp['t1']), sp['x0'], **murrayModel.solver_options)
 
+    #initialize circuit+flow pattern
+    # C = kfi.initialize_circuit_from_crystal('laves',3)
+    C = kfc.initialize_circuit_from_crystal('triagonal_planar',5).G
+    pars_src = {
+        'modesSRC': 'root_geometric'
+    }
+    pars_plx = {
+        'modePLX':'default',
+    }
     # set model and model parameters
-    pars_model={
+    pars_model = {
         'alpha_0':1.,
         'alpha_1':1.
     }
-    murrayModel = murray(pars_model)
-
     # # initialize dynamic system and set integration parameters
-    morpheus = gi.morph_dynamic(flow=F, model=murrayModel)
-
-    cnd = Circuit.edges['conductivity']
-    cnd_scale = Circuit.scales['conductance']
-    sp={
-        't0': 0.,
-        't1': 25.,
-        'x0': np.power(cnd/cnd_scale,0.25),
-    }
+    morpheus = gi.morph_dynamic(C, 'murray', [pars_model, pars_src, pars_plx])
+    morpheus.evals = 200
 
     # numerically evaluate the system
-    update = murrayModel.calc_update_stimuli
-    nsol = morpheus.nsolve(update, (sp['t0'],sp['t1']), sp['x0'], **murrayModel.solver_options)
+    cnd = morpheus.flow.circuit.edges['conductivity']
+    cnd_scale = morpheus.flow.circuit.scales['conductance']
 
-    return nsol, murrayModel, morpheus
+    sp = {
+        't0': 1e-05,
+        't1': 4.,
+        'x0': np.power(cnd/cnd_scale,0.25)*0.1,
+    }
+    nsol = morpheus.nlogSolve((sp['t0'],sp['t1']), sp['x0'])
+
+    return nsol, morpheus
 
 def get_nullity(H):
 
@@ -55,9 +84,10 @@ def get_nullity(H):
 
 def test_nsol():
 
-    nsol, murrayModel, morpheus = initEval()
+    nsol, morpheus = initEval()
 
-    dataPoints = zip(nsol.t, nsol.y.transpose())
+    dataPoints = zip(nsol.t,nsol.y.transpose())
+    murrayModel = morpheus.model
     args = murrayModel.solver_options['args']
     cost = [murrayModel.calc_cost_stimuli(t, y, *args)[0] for t, y in dataPoints]
 
